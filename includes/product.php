a8f9c78f518f697e682b858d65459dea24cc3425
Shiron <vannguyen11a21@gmail.com>
2025-10-09 01:15:32 +0700

    cteate file

---
+++ includes/product.php
@@ -0,0 +1,7 @@
+<?php 
+$products = [
+    1 => ['id' => 1, 'name' => 'Canvas Shoes', 'cost' => '500000'],
+    2 => ['id' => 2, 'name' => 'Nike Shoes', 'cost' => '1000000'],
+    3 => ['id' => 3, 'name' => 'Sport Shoes', 'cost' => '300000'],
+]
+?>
